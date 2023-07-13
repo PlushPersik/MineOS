fbd1ecf5fdea6b175203fc4a1f36d22f854054e8
PlushPersik <70471344+PlushPersik@users.noreply.github.com>
2023-07-14 00:54:19 +0800

    Update Installer.lua

--- Installer.lua
+++ Installer.lua
@@ -12,10 +12,10 @@ local args, options = shell.parse(...)
 ------------------------------------------------------------------------------------------------------------------------------------
 
 local URLs = {
-	applicationList = "https://raw.githubusercontent.com/IgorTimofeev/MineOS/master/Files.cfg",
-	installerLocalization = "https://raw.githubusercontent.com/IgorTimofeev/MineOS/master/Localizations/Installer/",
-	EFI = "https://raw.githubusercontent.com/IgorTimofeev/MineOS/master/EFI/Minified.lua",
-	license = "https://raw.githubusercontent.com/IgorTimofeev/MineOS/master/LICENSE",
+	applicationList = "https://raw.githubusercontent.com/PlushPersik/MineOS/master/Files.cfg",
+	installerLocalization = "https://raw.githubusercontent.com/PlushPersik/MineOS/master/Localizations/Installer/",
+	EFI = "https://raw.githubusercontent.com/PlushPersik/MineOS/master/EFI/Minified.lua",
+	license = "https://raw.githubusercontent.com/PlushPersik/MineOS/master/LICENSE",
 }
 
 ------------------------------------------------------------------------------------------------------------------------------------
